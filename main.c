d2031741848201b628cf03dfae28dd49637655c9
Anh <anh@fpt.edu.vn>
2018-10-05 15:29:43 +0700

    lam vc vs thu vien string

--- main.c
+++ main.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int conghaiso(int a, int b) {
-    return a + b;
-}
-
-int truhaiso(int a, int b) {
-    return a - b;
+void noiHoVaTen(char ho[], char ten[]) {
+    printf("Do dai cua ho la: %d\n", strlen(ho));
+    printf("Do dai cua ten la: %d\n", strlen(ten));
+    int dodaiHo = strlen(ho);
+    int dodaiTen = strlen(ten);
+    int tong = dodaiHo + dodaiTen;
+    printf("Do dai cua ca ho va ten la %d\n", tong);
+    tong++;
+    char hovaten[tong];
+    strcat(hovaten, ho);
+    strcat(hovaten, " ");
+    strcat(hovaten, ten);
+    printf("%s", hovaten);
 }
 
 int main() {
-    int *pointToArray = (int *) malloc(3 * sizeof(int));
-    if (pointToArray == NULL) {
-        printf("Not enough memory.");
-        return 0;
-    }
-    *pointToArray = 10;
-    pointToArray++;
-    *pointToArray = 20;
-    pointToArray++;
-    *pointToArray = 30;
-    for (int i = 0; i < 3; ++i) {
-        printf("%d\n", *pointToArray);
-        pointToArray--;
-    }
+    char ho[50], ten[50];
+    printf("Vui long nhap ho cua ban: ");
+    gets(ho);
+    // kiểm tra độ dài họ.
+    printf("Vui long nhap ten cua ban: ");
+    gets(ten);
+    // kiểm tra độ dài tên.
+    printf("Ho va ten cua ban la: ");
+    noiHoVaTen(ho, ten);
     return 0;
 }
