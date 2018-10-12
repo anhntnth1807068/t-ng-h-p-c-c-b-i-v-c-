547c84270fe61569cd31b4b8cb9aed4f7b815b1b
Anh <anh@fpt.edu.vn>
2018-10-12 22:10:12 +0700

    baitapthaygiao

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
-int main() {
-    printf("%-10s%-20s%-10s%-20s%-10s%-20s", "",  "Mã sinh viên", "|", "Tên sinh viên", "|", "Điện thoại");
-
+#include <stdbool.h>
+#include <math.h>
+bool chieudaicanhhuyen(int a,int b){
+    if(a<=0 || b <= 0){
+        printf("chieu dai cua  hai canh khong duoc bang khong");
+        return false;
+    }
+    return true;
+}
+float tinhcanhhuyen(int a,int b){
+    return (float)sqrt(pow(a,2) +pow(b,2));
+}
+int main(){
+    int a;
+    int b;
+    printf("vui long nhap canh thu nhat\n");
+    scanf("%d",&a);
+    printf("vui long nhap canh thu hai\n");
+    scanf("%d",&b);
+    bool kt=chieudaicanhhuyen(a,b);
+    if(kt==true){
+        printf("canh huyen cua tam giac %.2f",tinhcanhhuyen(a,b));
+    }
     return 0;
 }
