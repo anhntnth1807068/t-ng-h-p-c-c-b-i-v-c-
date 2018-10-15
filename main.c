c31315ec59fb4275d9a3b9ad814e13eb15009a4a
Anh <anh@fpt.edu.vn>
2018-10-15 18:25:44 +0700

    baitapve puts

--- main.c
+++ main.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-bool chieudaicanhhuyen(int a,int b){
-    if(a<=0 || b <= 0){
-        printf("chieu dai cua  hai canh khong duoc bang khong");
-        return false;
-    }
-    return true;
-}
-float tinhcanhhuyen(int a,int b){
-    return (float)sqrt(pow(a,2) +pow(b,2));
-}
+#include <memory.h>
 int main(){
-    int a;
-    int b;
-    printf("vui long nhap canh thu nhat\n");
-    scanf("%d",&a);
-    printf("vui long nhap canh thu hai\n");
-    scanf("%d",&b);
-    bool kt=chieudaicanhhuyen(a,b);
-    if(kt==true){
-        printf("canh huyen cua tam giac %.2f",tinhcanhhuyen(a,b));
-    }
+    char name[10];
+    printf("vui long nhap ten:\n");
+    fgets(name,sizeof(name)*sizeof(char),stdin);
+    printf("ten vua nhap la:");
+    puts(name);
     return 0;
+
 }
